b73c261d853a9f7fe401120c08f68780a3c07fb2
lisjim <heliss.code@gmail.com>
2022-08-25 19:53:02 -0500

    Ejercicio 1

--- LisethJimenez_Examen1.js
+++ LisethJimenez_Examen1.js
@@ -1,1 +1,20 @@
-hi
+var horasOrdinarias=40;
+var valorHoraNormal=265.00;
+var valorHoraExtra=350.00;
+//var horaPersona=53;
+
+function pagoHoras(horaPersona){
+    let pagoHoraOrdinaria=0;
+    let pagoHoraExtra=0;
+    let horasExtra = horaPersona-horasOrdinarias;
+    if(horaPersona > horasOrdinarias){
+        pagoHoraOrdinaria = horasOrdinarias*valorHoraNormal;
+        pagoHoraExtra = horasExtra*valorHoraExtra;
+    }
+    console.log("El total a pagar por horas laborales normal (40hrs) es de: "+pagoHoraOrdinaria);
+    console.log("El total a pagar por horas Extra  "+horasExtra+ "  hrs es de : "+pagoHoraExtra);
+}
+
+
+pagoHoras(53);
+
